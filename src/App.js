b95efe6d1f3c5d3d9f7485ba9cb0ea606e35ce60
chae <chae@chae.com>
2021-10-14 21:07:30 +0900

    state

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ import Content from './components/Content';
 import Subject from './components/Subject';
 
 class App extends Component {
+  constructor(props){ //제일먼저, 초기화담당
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub: 'World wide web!'},
+      contents:[
+        {id:1, title: 'HTML', desc: 'HTML is for information'},
+        {id:2, title: 'CSS', desc: 'CSS is for design'},
+        {id:3, title: 'JavaScript', desc: 'JavaScript is for interactive'}
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
         <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
