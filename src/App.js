9508f5d80740f554097f3a93fc9e0381db9d10e7
chae <chae@chae.com>
2021-10-16 05:18:07 +0900

    event

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ class App extends Component {
   constructor(props){ //제일먼저, 초기화담당
     super(props);
     this.state = {
+      mode:'read',
+      selected_content_id:2,
       subject:{title:'WEB', sub: 'World wide web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!'},
       contents:[
         {id:1, title: 'HTML', desc: 'HTML is for information'},
         {id:2, title: 'CSS', desc: 'CSS is for design'},
@@ -17,15 +20,42 @@ class App extends Component {
   }
 
   render() {
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode==='welcome'){
+      _title=this.state.welcome.title;
+      _desc=this.state.welcome.desc;
+    }else if(this.state.mode === 'read'){
+      var i = 0;
+      while(i<this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id===this.state.selected_content_id){
+          _title=data.title;
+          _desc=data.desc;
+          break;
+        }
+        i=i+1;
+      }
+    }
     return (
       <div className="App">
-        <Subject 
+        <Subject
           title={this.state.subject.title} 
-          sub={this.state.subject.sub}>
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            //alert('hihihi');
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+          >
         </Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <TOC onChangePage={function(id){
+          this.setState({
+            mode:'read',
+            selected_content_id:Number(id)
+          });
+        }.bind(this)} 
+        data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
